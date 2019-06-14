27cf26c9c0167f2dd0e4e22ededd9fca65ec078f
Ethan Claassen <ethan.claassen@lark-it.com>
2019-06-14 17:02:36 -0600

    Updated GPG keys and key server

--- manifests/gnupg_key.pp
+++ manifests/gnupg_key.pp
@@ -4,13 +4,13 @@ class rvm::gnupg_key(
   $key_id = $rvm::params::gnupg_key_id,
   $key_server = $rvm::params::key_server) inherits rvm::params {
 
-  #gnupg_key { "rvm_409B6B1796C275462A1703113804BB82D39DC0E3":
-  #  ensure     => present,
-  #  key_id     => "409B6B1796C275462A1703113804BB82D39DC0E3",
-  #  user       => 'root',
-  #  key_server => $key_server,
-  #  key_type   => public,
-  #}
+  gnupg_key { "rvm_3804BB82D39DC0E3":
+    ensure     => present,
+    key_id     => "3804BB82D39DC0E3",
+    user       => 'root',
+    key_server => $key_server,
+    key_type   => public,
+  }
 
   gnupg_key { "rvm_105BD0E739499BDB":
     ensure     => present,
